61158a0d61b3d3c9ccbd9da3434167613b7f1e72
Serhii Pylypenko <jpee.dev@gmail.com>
2021-06-13 16:16:45 +0300

    fix relative path
    add __dirname

--- src/info.js
+++ src/info.js
@@ -1,4 +1,8 @@
-import * as fs from 'fs';
+import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-export default JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf8'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8'));
